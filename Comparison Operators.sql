164017da72d6f477d52cd71887f5e61c7e2962d0
Sam Feighery <sfeighery@gmail.com>
2023-08-15 14:48:13 +0100

    Completed exercise

--- Comparison Operators.sql
+++ Comparison Operators.sql
@@ -8,17 +8,27 @@ WHERE gender = 'F' and state = 'OR'
 
 -- Who over the age of 44 has an income of 100 000 or more? (excluding 44)
 /*
-* Write your query here
+SELECT count(*) as "Result" fROM customers
+WHERE age > 44 and income >= '100000'
 */
+
+-- Returned 2497
 
 -- Who between the ages of 30 and 50 has an income less than 50 000?
 -- (include 30 and 50 in the results)
 
 /*
-* Write your query here
+SELECT count(*) as "Result" fROM customers
+WHERE (age >= 30 and age <= 50)
+and income < 50000
 */
+
+-- Returned 2362
 
 -- What is the average income between the ages of 20 and 50? (Excluding 20 and 50)
 /*
-* Write your query here
+SELECT avg( income ) as "Result" fROM customers
+WHERE (age > 20 and age < 50)
 */
+
+-- Returned 59409.926240780098
